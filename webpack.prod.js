7bb51165523e902892dfc1d7c3166039cbf0ca51
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-23 21:05:34 +0700

    Implement `babel-loader`

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,6 +6,22 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ]
+      }
+    ]
+  },
   optimization: {
     splitChunks: {
       chunks: 'all',
