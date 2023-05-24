beb5dbfc3a9c809b8635e77d0b86ffc71ba4031f
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-24 19:51:46 +0700

    Create `urlParser.js`

---
+++ src/scripts/routes/urlParser.js
@@ -0,0 +1,32 @@
+const urlParser = {
+  parseActiveUrlWithCombiner() {
+    const url = window.location.hash.slice(1).toLowerCase();
+    const splittedUrl = this._urlSplitter(url);
+
+    return this._urlCombiner(splittedUrl);
+  },
+
+  parseActiveUrlWithoutCombiner() {
+    const url = window.location.hash.slice(1).toLowerCase();
+
+    return this._urlSplitter(url);
+  },
+
+  _urlSplitter(url) {
+    const urlsSplits = url.split('/');
+
+    return {
+      resource: urlsSplits[1] || null,
+      id: urlsSplits[2] || null,
+      verb: urlsSplits[3] || null
+    };
+  },
+
+  _urlCombiner(splittedUrl) {
+    return (splittedUrl.resource ? `/${splittedUrl.resource}` : '/') +
+      (splittedUrl.id ? '/:id': '') +
+      (splittedUrl.verb ? `/${splittedUrl.verb}` : '');
+  }
+};
+
+export default urlParser;
