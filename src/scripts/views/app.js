6ac5bb9a06ad5a89e80a6c914cf76cdec5bc4b3c
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-24 19:53:43 +0700

    Create `App.js` + Add `async render`  method

---
+++ src/scripts/views/app.js
@@ -0,0 +1,28 @@
+import urlParser from '../routes/urlParser';
+import routes from '../routes/routes';
+
+/**
+ * Initialize application
+ * @class App
+ */
+class App {
+  /**
+   * @param {string} app
+   * Place your HTMLElement here. ex: document.getElementById('app').
+   */
+  constructor({
+    app
+  }) {
+    this._app = app;
+  }
+
+  // eslint-disable-next-line require-jsdoc
+  async render() {
+    const url = urlParser.parseActiveUrlWithCombiner();
+    const page = routes[url];
+
+    this._app.innerHTML = await page.render();
+  }
+};
+
+export default App;
