c75f89e633d44763f7a2ad58591733dc65b1d8b9
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-24 23:26:52 +0700

    Update Dashboard page + Adds Headliner + Adds image (placeholder) + Adds `Dashboard` button

--- src/scripts/views/pages/dashboard.js
+++ src/scripts/views/pages/dashboard.js
@@ -1,7 +1,15 @@
 const Dashboard = {
   async render() {
     return `
-      <h1>Tugas.ku</h1>
+      <div class="container__custom">
+        <h2>Manage your<br>task<br>with Tugas.ku</h2>
+        <img
+          src="https://www.purnomoyusgiantorocenter.org/wp-content/uploads/2020/06/placeholder.png"
+          loading="lazy"
+          alt="placeholder"
+        />
+        <a href="/" class="btn btn-primary">Dashboard</a>
+      </div>
     `;
   }
 };
