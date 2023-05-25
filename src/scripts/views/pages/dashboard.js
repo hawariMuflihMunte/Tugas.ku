bdab74feb534a458664ce500fd4e8edf7d226d08
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-25 13:48:26 +0700

    Implement "add task" feature in Dashboard page + This feature is still in development

--- src/scripts/views/pages/dashboard.js
+++ src/scripts/views/pages/dashboard.js
@@ -34,14 +34,83 @@ const Dashboard = {
           </button>
           <button
             type="button"
-            id="add-task"
             class="btn btn-warning"
             title="add task">
-            <span class="material-symbols-sharp">add</span>
+            <span class="material-symbols-sharp"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal">add</span>
           </button>
         </section>
         <section>
           <h2>Task</h2>
+
+          <!-- Add Task -->
+          <div
+            class="modal fade"
+            id="exampleModal"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1
+                    class="modal-title fs-5"
+                    id="exampleModalLabel">Add task</h1>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+                </div>
+                <form id="add-task" method="post">
+                  <div class="modal-body">
+                      <div class="input-group mb-3">
+                        <span class="input-group-text" id="title-input">#</span>
+                        <input
+                          id="title"
+                          type="text"
+                          class="form-control"
+                          placeholder="Title"
+                          aria-label="Title"
+                          aria-describedby="title-input" />
+                      </div>
+                      <div class="input-group mb-3">
+                        <textarea
+                          id="description"
+                          class="form-control"
+                          aria-label="Task Description"></textarea>
+                      </div>
+                      <div class="input-group mb-3">
+                        <span class="input-group-text">
+                          <span
+                            class="material-symbols-sharp">date_range</span>  
+                        </span>
+                        <input
+                          id="due-date"
+                          type="date"
+                          class="form-control"
+                          placeholder="Due Date"
+                          aria-label="Due Date"
+                          aria-describedby="due-date">
+                      </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button
+                      id="cancel-add-task"
+                      type="button"
+                      class="btn btn-danger"
+                      data-bs-dismiss="modal">Cancel</button>
+                    <button
+                      type="submit"
+                      class="btn btn-warning">Add Task</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          <!-- End Add Task -->
+
           <div id="task-list"></div>
         </section>
       </main>
@@ -55,6 +124,55 @@ const Dashboard = {
       event.stopImmediatePropagation();
       document.querySelector('.navigation__drawer')
           .classList.toggle('open__drawer');
+    });
+
+    const addTaskForm = document.querySelector('#add-task');
+
+    addTaskForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const title = document.querySelector('#title');
+      const description = document.querySelector('#description');
+      const dueDate = document.querySelector('#due-date');
+
+      if (title.value === '' || title.value === null) {
+        alert('Title is empty. Can\tt proceed');
+      }
+
+      if (dueDate.value === '' || dueDate.value === null) {
+        alert('Due Date is empty. Can\tt proceed');
+      }
+
+      const createCard = (data) => {
+        const {
+          title,
+          description,
+          dueDate
+        } = data;
+
+        const cardContainer = document.createElement('div');
+
+        cardContainer.innerHTML = `
+          <h3>${title}</h3>
+          <p>${description}</p>
+          <p>${dueDate}</p>
+        `;
+
+        return cardContainer;
+      };
+
+      const data = {
+        title: title.value,
+        description: description.value,
+        dueDate: dueDate.value
+      };
+
+      const card = createCard(data);
+
+      document.getElementById('task-list').append(card);
+
+      document.getElementById('cancel-add-task')
+          .dispatchEvent(new Event('click'));
     });
   }
 };
