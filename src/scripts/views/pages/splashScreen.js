8abb428bf7769f617430c1cd2c849756b7ea2eed
andriyan ginting <andrginting17@gmail.com>
2023-05-26 16:25:32 +0700

    Update splashscreen.js with latest changes

--- src/scripts/views/pages/splashScreen.js
+++ src/scripts/views/pages/splashScreen.js
@@ -12,43 +12,43 @@ const SplashScreen = {
         </div>
         <div class="col-lg-4">
           <h1 class=" mb-3">Manage your task with <span class="text-warning"> Tugasku</span> </h1>
-          <p class="lead">Lorem ipsum dolor sit amet consectetur. Massa fringilla gravida ipsum vel eget enim. Justo molestie </p>
+          <p class="lead">Don't let tasks pile up. Use our task management app to easily create lists, prioritize, and track progress.</p>
           <div class="d-grid gap-2 d-md-flex justify-content-md-start">
             <button type="button" class="btn btn-info btn-lg px-4 me-md-2 fw-bold">Dashboard</button>
           </div>
         </div>
       </div>
     </div>
-    <section class="benefit bg-body-tertiary pb-5">
+    <section class="benefit bg-light pb-5">
         <div class="container  g-5 py-5 mb-5">
           <div class="row  g-5 py-5 mb-5">
             <div class="col-12 text-center">
-              <h1>benefit</h1>
+              <h1>Benefit</h1>
             </div>
             <div class="col-lg-4">
               <div class="card text-center m-lg-5 m-3">
                 <div class="card-body p-4 my-3">
-                  <img src="" width="64" alt="">
+                  <img src="/icons/task-icon.svg" width="64" alt="">
                   <h5 class="pt-3 pb-3">Track Your Task</h5>
-                  <p>Lorem ipsum dolor sit amet consectetur. Massa fringilla gravida ipsum .</p>
+                  <p>A task management application helps you organize your work more efficiently .</p>
                 </div>
               </div>
             </div>
             <div class="col-lg-4">
               <div class="card text-center m-lg-5 m-3">
                 <div class="card-body p-4 my-3">
-                  <img src="" width="64" alt="">
-                  <h5 class="pt-3 pb-3">Track Your Task</h5>
-                  <p>Lorem ipsum dolor sit amet consectetur. Massa fringilla gravida ipsum .</p>
+                  <img src="/icons/chart-icon.svg" width="64" alt="">
+                  <h5 class="pt-3 pb-3">Increased Productivity</h5>
+                  <p> With a task management application, you can focus on the most important tasks and avoid less critical ones.</p>
                 </div>
               </div>
             </div>
             <div class="col-lg-4">
               <div class="card text-center m-lg-5 m-3">
                 <div class="card-body p-4 my-3">
-                  <img src="" width="64" alt="">
-                  <h5 class="pt-3 pb-3">Track Your Task</h5>
-                  <p>Lorem ipsum dolor sit amet consectetur. Massa fringilla gravida ipsum .</p>
+                  <img src="/icons/acces-icon.svg" width="64" alt="">
+                  <h5 class="pt-3 pb-3">Mobility and Accessibility</h5>
+                  <p>with task management applications you can access your tasks wherever you are.</p>
                 </div>
               </div>
             </div>
