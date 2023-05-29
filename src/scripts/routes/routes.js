72e9cf638ab5152c48e2271f080669f1df71b6f7
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-29 18:18:44 +0700

    Register details page in `routes.js`

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,10 +1,12 @@
 import Dashboard from '../views/pages/dashboard';
 import SplashScreen from '../views/pages/splashScreen';
+import Details from '../views/pages/details';
 
 const routes = {
   '/': SplashScreen,
   '/splash': SplashScreen,
-  '/dashboard': Dashboard
+  '/dashboard': Dashboard,
+  '/details/:id': Details
 };
 
 export default routes;
