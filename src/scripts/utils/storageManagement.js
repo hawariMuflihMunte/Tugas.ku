2eb65280cfdf0c42a8876c7756e7cb054b946ede
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-30 15:28:24 +0700

    JSDoc for `storageManagement` object

--- src/scripts/utils/storageManagement.js
+++ src/scripts/utils/storageManagement.js
@@ -1,4 +1,13 @@
+/**
+ * Front-end local & session storage tools.
+ */
 const storageManagement = {
+  /**
+   * @return {Boolean}
+   * - false
+   * If your web browser does not support Web Storage API.\
+   * Otherwise, return `true`.
+   */
   checkCompatibility() {
     if (typeof (Storage) === 'undefined') {
       console.error('Your browser does not support Web Storage API');
@@ -7,12 +16,23 @@ const storageManagement = {
     return true;
   },
 
+  /**
+   * @param {String} key
+   * Data key
+   * @param {String} data
+   * Value to be saved in local
+   */
   saveLocal(key, data) {
     if (this.checkCompatibility()) {
       localStorage.setItem(key, JSON.stringify(data));
     }
   },
 
+  /**
+   * @param {String} key
+   * Data key to be retrieved
+   * @return {Object}
+   */
   loadLocal(key) {
     if (this.checkCompatibility()) {
       const raw = localStorage.getItem(key);
@@ -20,12 +40,23 @@ const storageManagement = {
     }
   },
 
+  /**
+   * @param {String} key
+   * Data key
+   * @param {String} data
+   * Value to be saved in session
+   */
   saveSession(key, data) {
     if (this.checkCompatibility()) {
       sessionStorage.setItem(key, JSON.stringify(data));
     }
   },
 
+  /**
+   * @param {String} key
+   * Data key to be retrieved
+   * @return {Object}
+   */
   loadSession(key) {
     if (this.checkCompatibility()) {
       const raw = sessionStorage.getItem(key);
