c54f1ee8b0f755303f29581f6a0834c02fbe03f9
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-06-02 10:14:28 +0700

    Update `object` pass and display progress + Display progress rendered automatically if `object` changes occcurs

--- src/scripts/utils/card.js
+++ src/scripts/utils/card.js
@@ -11,15 +11,33 @@ import circularProgress from './circularProgress';
  */
 const createCard = (
     data,
-    cardLink = '#') => {
+    cardLink = '#'
+) => {
+  /*
+    {
+      id: Number(id),
+      createdAt: Date("DD-MM-YYYY"),
+      title: String(title),
+      description: String(description),
+      date: Date("DD-MM-YYYY"),
+      tasks: Array(tasks)
+    }
+  */
+
   const {
+    // eslint-disable-next-line no-unused-vars
+    id,
+    // eslint-disable-next-line no-unused-vars
+    createdAt,
     title,
     description,
-    dueDate
+    date,
+    // eslint-disable-next-line no-unused-vars
+    tasks
   } = data;
 
   const cardContainer = document.createElement('a');
-  cardContainer.classList.add('card__custom');
+  cardContainer.classList.add('card-custom');
   cardContainer.href = `${cardLink}`;
 
   const style = document.createElement('style');
@@ -28,7 +46,7 @@ const createCard = (
       text-decoration: none;
     }
 
-    .card__custom {
+    .card-custom {
       display: flex;
       justify-content: space-between;
       border-radius: 4px;
@@ -39,38 +57,43 @@ const createCard = (
       box-shadow: 0 1px 12px rgba(126, 126, 126, 0.32);
       cursor: pointer;
       width: 100%;
+      color: #777;
     }
 
-    .card__custom:focus,
-    .card__custom:hover {
+    .card-custom:focus,
+    .card-custom:hover {
       opacity: 0.85;
     }
 
-    .card__details {
-      padding-top: 11px;
-      padding-left: 11px;
+    .card-custom__details {
+      padding-top: 12px;
       min-width: 80%;
-      color: #666666;
+      color: inherit;
     }
 
-    .card__progress {
+    .card-custom:focus > .card-custom__details,
+    .card-custom:hover > .card-custom__details {
+      color: #666;
+    }
+
+    .card-custom__progress {
       display: flex;
       place-items: center;
       place-content: center;
       width: 100%;
-      height: 100;
+      height: 100%;
     }
 
-    .card__title {
+    .card-custom__title {
       font-weight: 500;
       margin-bottom: 7.5px;
     }
 
-    .card__description {
+    .card-custom__description {
       font-size: 16px;
     }
 
-    .card__date {
+    .card-custom__date {
       font-style: italic;
       color: #898989;
       font-size: 14px;
@@ -79,31 +102,47 @@ const createCard = (
   cardContainer.appendChild(style);
 
   const detailsData = document.createElement('div');
-  detailsData.classList.add('card__details');
+  detailsData.classList.add('card-custom__details');
 
   const detailsProgress = document.createElement('div');
-  detailsProgress.classList.add('card__progress');
+  detailsProgress.classList.add('card-custom__progress');
 
   const _title = document.createElement('h3');
   _title.textContent = title.toUpperCase();
-  _title.classList.add('card__title');
+  _title.classList.add('card-custom__title');
   detailsData.appendChild(_title);
 
   const _description = document.createElement('p');
   _description.textContent = description;
-  _description.classList.add('card__description');
+  _description.classList.add('card-custom__description');
   detailsData.appendChild(_description);
 
   const _dueDate = document.createElement('p');
-  const date = convertDate(dueDate, customMonthNames());
+  const _date = convertDate(date, customMonthNames());
 
-  _dueDate.textContent = date;
-  _dueDate.classList.add('card__date');
+  _dueDate.textContent = _date;
+  _dueDate.classList.add('card-custom__date');
   detailsData.appendChild(_dueDate);
 
   cardContainer.appendChild(detailsData);
 
-  const progress = circularProgress(99, 40);
+  let progressValue;
+  if (tasks.length === 0) {
+    progressValue = tasks.length;
+  }
+
+  if (tasks.length !== 0) {
+    const countUndoneTasks = tasks.filter((task) => !task.isDone).length;
+    const countDoneTasks = tasks.filter((task) => task.isDone).length;
+
+    console.log(countUndoneTasks);
+    console.log(countDoneTasks);
+
+    const setValueToProgress = countDoneTasks / tasks.length * 100;
+    progressValue = setValueToProgress;
+  }
+
+  const progress = circularProgress(progressValue, 75);
 
   detailsProgress.appendChild(progress);
   cardContainer.appendChild(progress);
