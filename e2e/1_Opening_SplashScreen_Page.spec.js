d6409e8c7513abec3f9929898b834758a474deef
Ridwan Miftahul Falah <miftahridwan1101@gmail.com>
2023-06-09 17:11:16 +0700

    Update `1_Opening_SplashScreen_Page.spec.js` (not finished)

---
+++ e2e/1_Opening_SplashScreen_Page.spec.js
@@ -0,0 +1,12 @@
+/* eslint-disable object-curly-spacing */
+/* eslint-disable new-cap */
+Feature('Opening SplashScreen Page');
+
+Before(({ I }) => {
+  I.amOnPage('/');
+});
+
+Scenario('Showing Jumbotron', async ({ I }) => {
+  I.wait(2);
+  I.seeElement(locate('div.container'));
+});
