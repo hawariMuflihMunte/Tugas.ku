aeee1a77946b913469a6cc49eb7da75bf3bb18bf
Ridwan Miftahul Falah <miftahridwan1101@gmail.com>
2023-06-13 17:20:10 +0700

    Refactor `Add a New Task` testing scenario

--- e2e/Task_CRUD.spec.js
+++ e2e/Task_CRUD.spec.js
@@ -1,21 +1,30 @@
+/* eslint-disable max-len */
 /* eslint-disable linebreak-style */
 /* eslint-disable new-cap */
-Feature('Add task');
+Feature('Create, Read, Update and Delete a Task');
 
 Before(({I}) => {
   I.amOnPage('/#/dashboard');
 });
 
-Scenario('Showing empty task in dashboard', async ({I}) => {
-  // pause();
-  I.wait(3);
+Scenario('Add a New Task', async ({I}) => {
+  I.say('I am going to wait for Add New Task button to render, and then click it.');
+  I.waitForElement('button[title="add task"]');
   I.click(locate('button[title="add task"]'));
 
-  I.wait(3);
-  I.fillField('#title', 'Task Title');
+  I.say('I am going to wait for Add New Task form to render');
+  I.waitForElement('div.modal-content');
+
+  I.say('I am going to input the Task Title');
+  I.seeElement('input#title');
+  I.fillField('input#title', 'Task Title Testing');
+
+  I.say('I am going to add a Sub-task');
   I.click(locate('button#add-task-item'));
   I.seeElement(locate('div#task-items'));
-  I.fillField(locate('input[name=\'task\''), 'subtask');
+  I.fillField(locate('input[name=\'task\''), 'Sub-Task Testing');
+
+  I.say('I am going to input the Task Description');
   I.seeElement(locate('textarea#description'));
   I.fillField(locate('textarea#description'), `
     Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -23,25 +32,23 @@ Scenario('Showing empty task in dashboard', async ({I}) => {
     id explicabo eius nulla corporis qui labore sequi.
   `.replace(/\s+/g, ' '));
 
-
-  I.wait(3);
-
   const today = new Date();
   const year = today.getFullYear();
   const month = String(today.getMonth() + 1).padStart(2, '0');
   const day = String(today.getDate()).padStart(2, '0');
   const dueDate = `${day}-${month}-${year}`;
 
+  I.say('I am going to input the Task Due-date');
   I.fillField('#due-date', dueDate);
+
+  I.say('I am going to add the new Task');
   I.pressKey('Enter');
 
+  I.say('I expect to see a succesful message modal and then close it');
   I.seeElement(locate('.swal2-confirm'));
   I.click(locate('.swal2-confirm'));
 
-  I.wait(1);
+  I.say('I am going to close the Task Form');
   I.seeElement(locate('button[data-bs-dismiss=\'modal\']'));
   I.click(locate('button[data-bs-dismiss=\'modal\']'));
-
-  // I.wait(2);
-  // I.see('Task Title', 'h3.card-custom__title');
 });
